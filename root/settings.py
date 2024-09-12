8fe9905654385c52c7b78a3069ef03b8c5d98b95
xolmomin <xolmomin@gmail.com>
2024-09-12 17:57:14 +0500

    add github cicd

--- root/settings.py
+++ root/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
 }
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'GROUP API p21',
+    'TITLE': 'GROUP API p21 123',
     'DESCRIPTION': 'Your project description',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
